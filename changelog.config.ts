b086485e5d698f8983d85da8db91634718d29a49
qdcz <532911513@qq.com>
2022-12-27 23:19:49 +0800

    feat: 🎸 git cz

--- changelog.config.ts
+++ changelog.config.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
     disableEmoji: false,
     format: "{type}{scope}: {emoji}{subject}",
     list: ["test", "feat", "fix", "chore", "docs", "refactor", "style", "ci", "perf",],
